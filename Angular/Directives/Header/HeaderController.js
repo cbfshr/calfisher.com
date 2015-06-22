d9c8f7030f5c5ff11ec64e4be1f86a6867552304
Cal Fisher <cfisher@iastate.edu>
2015-06-22 18:05:00 -0500

    Initial commit for CalFisher.com

---
+++ Angular/Directives/Header/HeaderController.js
@@ -0,0 +1,11 @@
+﻿angular
+	.module('app.controllers')
+	.controller('HeaderController', [
+		'$scope', '$location',
+		function ($scope, $location) {
+			$scope.changePath = function (path) {
+				var _this = this;
+				//_this.$location.path(path);
+			};
+		}
+	]);
---
+++ Angular/Pages/Home/HomeCtrl.js
@@ -0,0 +1,50 @@
+﻿var Home;
+(function (Home) {
+	var Controller = (function () {
+		function Controller($scope, $http, $location, $routeParams, $filter, $anchorScroll) {
+			this.$scope = $scope;
+			this.$http = $http;
+			this.$location = $location;
+			this.$routeParams = $routeParams;
+			//$scope.$dialog = $dialog;
+			$scope.$filter = $filter;
+			//this.$navigation = $navigation;
+			//this.$util = $util;
+			this.$anchorScroll = $anchorScroll;
+			var _this = this;
+
+
+			$scope.sectionSelected = {
+				'cf': false,
+				'about': false,
+				'projects': false,
+				'contact': false
+			};
+
+			$scope.changePage = function () {
+				return _this.changePage();
+			};
+		}
+
+		Controller.$inject = [
+			'$scope',
+			'$http',
+			'$location',
+			'$routeParams',
+			//'$dialog',
+			'$filter',
+			//'$navigation',
+			//'$util',
+			'$anchorScroll'
+		];
+
+		Controller.prototype.changePage = function () {
+			var _this = this;
+
+			_this.$location.path('/Contact');
+		};
+
+		return Controller;
+	})();
+	Home.Controller = Controller;
+})(Home || (Home = {}));
